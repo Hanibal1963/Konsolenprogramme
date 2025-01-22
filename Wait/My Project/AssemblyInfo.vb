006f6047626a75b7d17d7f90135253eb7c8dbeae
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-01-22 20:56:05 +0100

    Neues Projekt "Wait" hinzugefügt
    Ein neues Projekt "Wait" wurde zur Lösung "Konsolenprogramme.sln" hinzugefügt. Die Projektdateien umfassen Konfigurationsdateien wie "App.config", "Application.Designer.vb", "Application.myapp", "AssemblyInfo.vb", "Resources.Designer.vb", "Resources.resx", "Settings.Designer.vb", "Settings.settings" und "app.manifest". Der Hauptcode für das Konsolenprogramm befindet sich in "Programm.vb".
    
    Die Projektdatei "Wait.vbproj" wurde erstellt und enthält XML-Deklarationen, verschiedene `<PropertyGroup>`-Elemente für Projekteigenschaften und Konfigurationen, `<ItemGroup>`-Elemente für Referenzen, Importe, Kompilierungselemente, eingebettete Ressourcen und andere Dateien sowie ein `<Import>`-Element für MSBuild-Ziele.

---
+++ Wait/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Wait")>
+<Assembly: AssemblyDescription("Konsolenprogramm hält die Batchverarbeitung für eine Anzahl von Sekunden an.")>
+<Assembly: AssemblyCompany("SchlumpfSoft")>
+<Assembly: AssemblyProduct("Wait")>
+<Assembly: AssemblyCopyright("Copyright © Andreas Sauer 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("7073521d-d415-4b9b-aed6-2152fef59b3a")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.2025.01.22")>
+<Assembly: AssemblyFileVersion("1.2025.01.22")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
---
+++ Wait/Programm.vb
@@ -0,0 +1,110 @@
+﻿' ****************************************************************************************************************
+' Programm.vb
+' © 2025 by Andreas Sauer
+'
+' Beschreibung:
+' Konsolenprogramm welches die Abarbeitung einer Batch Datei für eine festgelegte Zeit anhält.
+'
+' Verwendung:
+' Wait /n:[NN]
+'
+' Parameter NN:
+' Wartezei in Sekunden (0-99)
+'
+' ****************************************************************************************************************
+'
+
+Imports System.Timers
+
+Module Programm
+
+	Private name As String
+	Private version As String
+	Private copyright As String
+	Private otimer As Timer
+	Private timeout As Boolean
+
+	Sub Main(Args As String())
+		'Programmheader anzeigen
+		ShowAppHeader()
+		'Anzahl der übergebenen Argumente prüfen
+		If Args.Length = 0 Then
+			'Fehlermeldung anzeigen
+			Console.WriteLine($"Fehler in der Kommandozeile.")
+			Console.WriteLine($"Es wurden keine Parameter angegeben")
+			'Hilfe anzeigen
+			ShowHelp()
+		Else
+			'wenn Argumente angegeben wurden -> 1. Argument einlesen
+			Dim Arg As String = Args(0)
+			If InStr(Arg, "/n:", CompareMethod.Text) > 0 Then
+				'Argument ist richtig -> Wert des Arguments ermitteln
+				Dim ArgWert As String = Split(Arg, ":", -1, CompareMethod.Text)(1)
+				'Fehlertest des Argumentwertes
+				If ArgWert <> "" Then
+					'Argumentwert ist nicht leer -> in Zahl wandeln
+					Dim Wert As Integer = CInt(ArgWert)
+					If Wert > 0 Then
+						'Argumentwert ist > 0 ->  Test ob > 99
+						If Wert > 99 Then
+							'Wert ist > 99 -> auf 99 zurücksetzen
+							Wert = 99
+						End If
+						'Timer setzen
+						SetTimer(Wert)
+						'auf Timeout warten
+						timeout = False
+						Do Until timeout = True
+						Loop
+						'Programmende
+						Exit Sub
+					Else
+						'Argumentwert ist 0 oder kleiner -> Programmende
+						Exit Sub
+					End If
+				Else
+					'Argumentwert ist leer -> Programmende
+					Exit Sub
+				End If
+			Else
+				'Argument ist fehlerhaft -> Fehlermeldung ausgeben
+				Console.WriteLine($"Fehler !")
+				Console.WriteLine($"Das angegebene Argument ""{Arg}"" ist fehlerhaft {vbCrLf}")
+				'Hilfe anzeigen
+				ShowHelp()
+				'Programmende
+				Exit Sub
+			End If
+		End If
+	End Sub
+
+	Private Sub SetTimer(Wert As Integer)
+		'neuen Timer erstellen
+		otimer = New Timer(Wert * 1000)
+		'Eventbearbeitung setzen
+		AddHandler otimer.Elapsed, AddressOf OnTimerEvent
+		'Timer starten
+		otimer.Enabled = True
+	End Sub
+
+	Private Sub OnTimerEvent(sender As Object, e As ElapsedEventArgs)
+		'Zeit abgelaufen
+		timeout = True
+	End Sub
+
+	Private Sub ShowHelp()
+		Console.WriteLine($"")
+		Console.WriteLine($"Verwendungsbeispiel:")
+		Console.WriteLine($"{name} /n:[NN]")
+		Console.WriteLine("")
+		Console.WriteLine()
+	End Sub
+
+	Private Sub ShowAppHeader()
+    name = My.Application.Info.AssemblyName
+    version = My.Application.Info.Version.ToString
+    copyright = My.Application.Info.Copyright
+    Console.WriteLine($"{name} V{version}{vbCrLf}{copyright}")
+  End Sub
+
+End Module
