8dc0685f4e2f51f0b490ab31e572ba2d22220a16
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-01-23 20:32:19 +0100

    Verbesserung der Argumentverarbeitung und Fehlerbehandlung
    Option Strict On hinzugefügt für strengere Typüberprüfungen.
    Neue Konstanten MaxTones und MinTones definiert.
    Parameterbezeichnung in Main von args zu Args geändert.
    Argumentverarbeitung überarbeitet:
      - Fehlermeldung und Programmende bei fehlenden Argumenten.
      - Überprüfung und Extraktion des Arguments bei /n:.
      - Begrenzung des Werts auf MinTones und MaxTones.
      - Ausgabe der Töne basierend auf der Anzahl.
      - Fehlermeldungen bei ungültigen Argumenten/Werten.
    Neue Methode ShowErrorMsg für Fehlermeldungen und Hilfe.
    ShowHelp und ShowAppHeader leicht angepasst.

--- Beep/Programm.vb
+++ Beep/Programm.vb
@@ -13,81 +13,81 @@
 '
 ' ****************************************************************************************************************
 '
+Option Strict On
 
 Module Programm
+
+	Private Const MaxTones As Integer = 99
+	Private Const MinTones As Integer = 0
 
 	Private name As String
 	Private version As String
 	Private copyright As String
 
-	Sub Main(args As String())
+	Sub Main(Args As String())
+
 		'Anzahl der übergebenen Argumente prüfen
-		If args.Length = 0 Then
-			'Programmheader anzeigen
-			ShowAppHeader()
-			'Fehlermeldung anzeigen
-			Console.WriteLine($"Fehler in der Kommandozeile.")
-			Console.WriteLine($"Es wurden keine Parameter angegeben")
-			'Hilfe anzeigen
-			ShowHelp()
-		Else
-			'wenn Argumente angegeben wurden -> 1. Argument einlesen
-			Dim Arg As String = args(0)
-			'Fehlertest des eingelesenen Arguments
-			If InStr(Arg, "/n:", CompareMethod.Text) > 0 Then
-				'Argument ist richtig -> Wert des Arguments ermitteln
-				Dim ArgWert As String = Split(Arg, ":", -1, CompareMethod.Text)(1)
-				'Fehlertest des Argumentwertes
-				If ArgWert <> "" Then
-					'Argumentwert ist nicht leer -> in Zahl wandeln
-					Dim Wert As Integer = CInt(ArgWert)
-					If Wert > 0 Then
-						'Argumentwert ist > 0 ->  Test ob > 99
-						If Wert > 99 Then
-							'Wert ist > 99 -> auf 99 zurücksetzen
-							Wert = 99
-						End If
-						'Ausgabe der Töne
-						For n As Integer = 1 To Wert
-							Console.Beep()
-						Next
-						'Programmende
-						Exit Sub
-					Else
-						'Argumentwert ist 0 oder kleiner -> Programmende
-						Exit Sub
-					End If
-				Else
-					'Argumentwert ist leer -> Programmende
-					Exit Sub
-				End If
-			Else
-				'Programmheader anzeigen
-				ShowAppHeader()
-				'Argument ist fehlerhaft -> Fehlermeldung ausgeben
-				Console.WriteLine($"Fehler !")
-				Console.WriteLine($"Das angegebene Argument ""{Arg}"" ist fehlerhaft {vbCrLf}")
-				'Hilfe anzeigen
-				ShowHelp()
-				'Programmende
-				Exit Sub
-			End If
+		If Args.Length = 0 Then
+
+			'Fehlermeldung anzeigen wenn keine Argumente angegeben sind	und Ende
+			ShowErrorMsg($"Es wurden keine Parameter angegeben!")
+			Return
+
 		End If
+
+		'Wenn Argumente angegeben wurden -> 1. Argument einlesen
+		Dim Arg As String = Args(0)
+
+		'Fehlertest des eingelesenen Arguments
+		If Arg.StartsWith("/n:", StringComparison.OrdinalIgnoreCase) Then
+
+			' Wert des Arguments ermitteln
+			Dim ArgWert As String = Arg.Substring(3)
+
+			' Fehlertest des Argumentwertes
+			Dim Wert As Integer
+			If Integer.TryParse(ArgWert, Wert) Then
+
+				' Argumentwert ist gültig -> Wert begrenzen
+				Wert = Math.Max(MinTones, Math.Min(MaxTones, Wert))
+
+				' Ausgabe der Töne
+				For n As Integer = 0 To Wert
+					Console.Beep()
+				Next
+
+			Else
+
+				' Ungültiger Zahlenwert
+				ShowErrorMsg($"Der angegebene Wert ist keine gültige Zahl.")
+
+			End If
+
+		Else
+
+			' Argument ist fehlerhaft -> Fehlermeldung ausgeben
+			ShowErrorMsg($"Das angegebene Argument ""{Arg}"" ist fehlerhaft!")
+
+		End If
+
+	End Sub
+
+	Private Sub ShowErrorMsg(Message As String)
+		' Programmheader anzeigen
+		ShowAppHeader()
+		Console.WriteLine($"Fehler!{vbCrLf}{Message}{vbCrLf}")
+		ShowHelp()
 	End Sub
 
 	Private Sub ShowHelp()
-		Console.WriteLine($"")
-		Console.WriteLine($"Verwendungsbeispiel:")
-		Console.WriteLine($"{name} /n:[NN]")
-		Console.WriteLine("")
-		Console.WriteLine()
+		Console.WriteLine($"Verwendungsbeispiel:{vbCrLf}{name} /n:[NN]{vbCrLf}")
 	End Sub
 
 	Private Sub ShowAppHeader()
 		name = My.Application.Info.AssemblyName
 		version = My.Application.Info.Version.ToString
 		copyright = My.Application.Info.Copyright
-		Console.WriteLine($"{name} V{version}{vbCrLf}{copyright}")
+		Console.WriteLine($"{name} V{version}{vbCrLf}{copyright}{vbCrLf}")
 	End Sub
 
 End Module
