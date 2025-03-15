e433dbb860c33cd560a9e6e065285fd5ac9d6275
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-03-15 11:10:54 +0100

    Neues Konsolenanwendungsprojekt BeepNet erstellt
    Erstellt das Projekt `BeepNet` mit Unterstützung für die Ziel-Frameworks net8.0 und net481.
    Fügt Konfigurationseigenschaften in `BeepNet.vbproj` hinzu und definiert Debug- und Release-PropertyGroups.
    Ein neues Profil in `launchSettings.json` ermöglicht die Ausführung mit Befehlszeilenargumenten.
    In `Program.vb` wurde ein Konsolenprogramm implementiert, das Signaltöne ausgibt, einschließlich Argumentvalidierung und Hilfsfunktionen für Fehlermeldungen und Anwendungsinformationen.

---
+++ BeepNet/Program.vb
@@ -0,0 +1,87 @@
+' ****************************************************************************************************************
+' Programm.vb
+' © 2025 by Andreas Sauer
+'
+' Beschreibung: 
+' Konsolenprogramm zum ausgeben von Signaltönen.
+'
+' Verwendung:
+' Beep /n:[NN]
+'
+' Parameter NN:
+' Anzahl der Töne (0-99)
+'
+' ****************************************************************************************************************
+'
+
+Imports System.Reflection
+
+Module Programm
+
+	Private Const MaxTones As Integer = 99
+	Private Const MinTones As Integer = 0
+	Private name As String
+	Private version As String
+	Private copyright As String
+
+	Sub Main(Args As String())
+		'Anzahl der übergebenen Argumente prüfen
+		If Args.Length = 0 Then
+			'Fehlermeldung anzeigen wenn keine Argumente angegeben sind	und Ende
+			ShowErrorMsg($"Es wurden keine Parameter angegeben!")
+			Return
+		End If
+		'Wenn Argumente angegeben wurden -> 1. Argument einlesen
+		Dim Arg As String = Args(0)
+		'Fehlertest des eingelesenen Arguments
+		If Arg.StartsWith("/n:", StringComparison.OrdinalIgnoreCase) Then
+			' Wert des Arguments ermitteln
+			Dim ArgWert As String = Split(Arg, ":", -1, CompareMethod.Text)(1)
+			' Fehlertest des Argumentwertes
+			Dim Wert As Integer
+			If Integer.TryParse(ArgWert, Wert) Then
+				' Argumentwert ist gültig -> Wert begrenzen
+				Wert = Math.Max(MinTones, Math.Min(MaxTones, Wert))
+				' Ausgabe der Töne
+				For n As Integer = 0 To Wert
+					Console.Beep()
+				Next
+			Else
+				' Ungültiger Zahlenwert
+				ShowErrorMsg($"Der angegebene Wert ist keine gültige Zahl.")
+			End If
+		Else
+			' Argument ist fehlerhaft -> Fehlermeldung ausgeben
+			ShowErrorMsg($"Das angegebene Argument ""{Arg}"" ist fehlerhaft!")
+		End If
+	End Sub
+
+	Private Sub ShowErrorMsg(Message As String)
+		' Programmheader anzeigen
+		ShowAppHeader()
+		Console.WriteLine($"Fehler!{vbCrLf}{Message}{vbCrLf}")
+		ShowHelp()
+	End Sub
+
+	Private Sub ShowHelp()
+		Console.WriteLine($"Verwendungsbeispiel:{vbCrLf}{name} /n:[NN]{vbCrLf}")
+	End Sub
+
+	Private Sub ShowAppHeader()
+		name = Assembly.GetExecutingAssembly().GetName().Name
+		version = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+		copyright = GetAssemblyCopyright()
+		Console.WriteLine($"{name} V{version}{vbCrLf}{copyright}{vbCrLf}")
+	End Sub
+
+	Private Function GetAssemblyCopyright() As String
+		Dim assembly As Assembly = Assembly.GetExecutingAssembly()
+		Dim attributes As Object() = assembly.GetCustomAttributes(GetType(AssemblyCopyrightAttribute), False)
+		If attributes.Length > 0 Then
+			Dim copyrightAttribute As AssemblyCopyrightAttribute = CType(attributes(0), AssemblyCopyrightAttribute)
+			Return copyrightAttribute.Copyright
+		End If
+		Return String.Empty
+	End Function
+
+End Module
