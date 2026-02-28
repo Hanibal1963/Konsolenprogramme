002b2c17fd9a78426c2487c7b6fda0ee7f7e50c0
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-02-28 12:08:13 +0100

    Ressourcen zentralisiert & Fehlerausgabe refaktoriert
    - Neue strongly-typed Properties für Fehlermeldungen und Hilfetext in Resources.Designer.vb ergänzt
    - Ressourcen ErrorMsg_01, ErrorMsg_02, ErrorMsg_03 und HelpMsg in Resources.resx angelegt und mit deutschen Texten versehen
    - XML-Schema und .NET-Komponenten-Versionen in Resources.resx aktualisiert
    - Verweis auf ConsoleHelperLib im Projekt hinzugefügt
    - Fehler- und Hilfetextausgabe in Program.vb entfernt und durch ConsoleHelper.ShowErrorMsg ersetzt
    - Argumentprüfung nutzt jetzt Ressourcen für konsistente Fehlermeldungen
    - Benutzertexte vollständig in Ressourcen ausgelagert für bessere Wartbarkeit und Internationalisierung

--- Power/Program.vb
+++ Power/Program.vb
@@ -14,28 +14,25 @@
 ' - /hibernate -> Ruhezustand
 ' --------------------------------------------------------------------------------------------------------
 
-Imports System.Reflection
-
 Module Program
-
-    Private name As String
-    Private version As String
-    Private copyright As String
 
     Sub Main(Args As String())
 
         ' Anzahl der übergebenen Argumente prüfen
         If Args.Length = 0 Then
             ' Fehlermeldung anzeigen wenn keine Argumente angegeben sind	und Ende
-            ShowErrorMsg($"Es wurden keine Parameter angegeben!")
+            ConsoleHelper.ShowErrorMsg(My.Application.Info, $"{My.Resources.ErrorMsg_01}", $"{My.Resources.HelpMsg}")
             Return
+
         ElseIf Args.Length = 1 Then
             ' Parameter prüfen
             ScanArgs(Args(0))
+
         ElseIf Args.Length > 1 Then
             ' Fehlermeldung anzeigen wenn zu viele Argumente angegeben sind	und Ende
-            ShowErrorMsg($"Es wurden zu viele Parameter angegeben!")
+            ConsoleHelper.ShowErrorMsg(My.Application.Info, $"{My.Resources.ErrorMsg_02}", $"{My.Resources.HelpMsg}")
             Return
+
         End If
 
     End Sub
@@ -43,67 +40,37 @@ Module Program
     Private Sub ScanArgs(Arg As String)
 
         Select Case Arg.ToLower()
+
             Case "/logoff"
                 ' Benutzer abmelden
                 WindowsController.ExitWindows(RestartOptions.LogOff, False)
+
             Case "/poweroff"
                 ' Herunterfahren und ausschalten
                 WindowsController.ExitWindows(RestartOptions.PowerOff, False)
+
             Case "/shutdown"
                 ' Herunterfahren und ausschalten
                 WindowsController.ExitWindows(RestartOptions.ShutDown, False)
+
             Case "/reboot"
                 ' Herunterfahren und neu starten
                 WindowsController.ExitWindows(RestartOptions.Reboot, False)
+
             Case "/suspend"
                 ' Energiesparmodus
                 WindowsController.ExitWindows(RestartOptions.Suspend, False)
+
             Case "/hibernate"
                 ' Ruhezustand
                 WindowsController.ExitWindows(RestartOptions.Hibernate, False)
+
             Case Else
                 ' Fehlermeldung anzeigen wenn ungültiger Parameter angegeben ist	und Ende
-                ShowErrorMsg($"Der angegebene Parameter '{Arg}' ist ungültig!")
+                ConsoleHelper.ShowErrorMsg(My.Application.Info, $"{My.Resources.ErrorMsg_03}", $"{My.Resources.HelpMsg}")
                 Return
+
         End Select
     End Sub
-
-    Private Sub ShowErrorMsg(Message As String)
-        ' Programmheader anzeigen
-        ShowAppHeader()
-        Console.WriteLine($"Fehler!{vbCrLf}{Message}{vbCrLf}")
-        ShowHelp()
-    End Sub
-
-    Private Sub ShowHelp()
-        Console.WriteLine($"Verwendungsbeispiel:{vbCrLf}{name} /opt{vbCrLf}")
-        Console.WriteLine($"mögliche Werte für /opt sind:{vbCrLf}")
-        Console.WriteLine($"/logoff -> Benutzer abmelden")
-        Console.WriteLine($"/poweroff -> Herunterfahren und ausschalten")
-        Console.WriteLine($"/reboot -> Herunterfahren und neu starten")
-        Console.WriteLine($"/shutdown -> Herunterfahren und ausschalten")
-        Console.WriteLine($"/suspend -> Energiesparmodus")
-        Console.WriteLine($"/hibernate -> Ruhezustand")
-    End Sub
-
-    Private Sub ShowAppHeader()
-        'name = My.Application.Info.AssemblyName
-        name = Assembly.GetExecutingAssembly().GetName().Name
-        'version = My.Application.Info.Version.ToString
-        version = Assembly.GetExecutingAssembly().GetName().Version.ToString()
-        'copyright = My.Application.Info.Copyright
-        copyright = GetAssemblyCopyright()
-        Console.WriteLine($"{name} V{version}{vbCrLf}{copyright}{vbCrLf}")
-    End Sub
-
-    Private Function GetAssemblyCopyright() As String
-        Dim assembly As Assembly = Assembly.GetExecutingAssembly()
-        Dim attributes As Object() = assembly.GetCustomAttributes(GetType(AssemblyCopyrightAttribute), False)
-        If attributes.Length > 0 Then
-            Dim copyrightAttribute As AssemblyCopyrightAttribute = CType(attributes(0), AssemblyCopyrightAttribute)
-            Return copyrightAttribute.Copyright
-        End If
-        Return String.Empty
-    End Function
 
 End Module
