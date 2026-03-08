3478c064ea46f1af47e75791503f0824c5164d54
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-03-08 11:47:56 +0100

    Verbesserte Doku & Struktur in ConsoleHelper, .gitignore
    - MigrationBackup/ zu .gitignore hinzugefügt
    - Ausführlicher Header und XML-Dokumentation in ConsoleHelper.vb ergänzt
    - Methoden in ConsoleHelper umstrukturiert und vereinheitlicht
    - Auskommentierte Altlogik entfernt
    - Konsolenprogramme.sln.vsdoc zu SolutionItems hinzugefügt

--- ConsoleHelperLib/ConsoleHelper.vb
+++ ConsoleHelperLib/ConsoleHelper.vb
@@ -1,47 +1,74 @@
-﻿
+﻿' --------------------------------------------------------------------------------------------------------
+' Datei: ConsoleHelper.vb
+' Author: Andreas Sauer
+' Datum: 08.03.2026
+' Beschreibung:
+' Hilfsfunktionen für die Ausgabe von Fehlermeldungen, Hilfemeldungen und Programmheader in der Konsole.
+' --------------------------------------------------------------------------------------------------------
+
 Imports Microsoft.VisualBasic.ApplicationServices
 
+''' <summary>
+''' Diese Klasse definiert Hilfsfunktionen für die Ausgabe von Fehlermeldungen,
+''' <br/>
+''' Hilfemeldungen und Programmheader in der Konsole.
+''' </summary>
 Public Class ConsoleHelper
 
+    ''' <summary>
+    ''' Zeigt den Programmheader, eine Fehlermeldung und eine Hilfemeldung in der
+    ''' Konsole an.
+    ''' </summary>
+    ''' <param name="ErrorMsg">Die anzuzeigende Fehlermeldung.</param>
+    ''' <param name="HelpMsg">Die anzuzeigende Hilfemeldung.</param>
+    ''' <param name="AppInfo">Die Assembly-Informationen der Anwendung.</param>
     Public Shared Sub ShowErrorMsg(ErrorMsg As String, HelpMsg As String, AppInfo As AssemblyInfo)
-        '' Programmheader anzeigen
-        'Dim name = AppInfo.AssemblyName
-        'Dim version = AppInfo.Version.ToString
-        'Dim copyright = AppInfo.Copyright
-
-        'Console.WriteLine($"{name} V{version}{vbCrLf}{copyright}{vbCrLf}")
-
-        ShowAppInfo(AppInfo)
-
-        '' Fehlermeldung anzeigen
-        'Console.WriteLine($"{ErrorMsg}")
-        ShowErrorMsg(ErrorMsg)
-
-        '' Hilfemeldung anzeigen
-        'Console.WriteLine($"{HelpMsg}")
-
-        ShowHelpMsg(HelpMsg)
-
+        ShowAppInfo(AppInfo) ' Programmheader anzeigen
+        ShowErrorMsg(ErrorMsg) ' Fehlermeldung anzeigen
+        ShowHelpMsg(HelpMsg)        ' Hilfemeldung anzeigen
     End Sub
 
+    ''' <summary>
+    ''' Zeigt eine Fehlermeldung und eine Hilfemeldung in der Konsole an.
+    ''' </summary>
+    ''' <param name="ErrorMsg">Die anzuzeigende Fehlermeldung.</param>
+    ''' <param name="HelpMsg">Die anzuzeigende Hilfemeldung.</param>
     Public Shared Sub ShowErrorMsg(ErrorMsg As String, HelpMsg As String)
         ShowErrorMsg(ErrorMsg)
         ShowHelpMsg(HelpMsg)
     End Sub
 
+    ''' <summary>
+    ''' Zeigt eine Fehlermeldung und den Programmheader in der Konsole an.
+    ''' </summary>
+    ''' <param name="ErrorMsg">Die anzuzeigende Fehlermeldung.</param>
+    ''' <param name="AppInfo">Die Assembly-Informationen der Anwendung.</param>
     Public Shared Sub ShowErrorMsg(ErrorMsg As String, AppInfo As AssemblyInfo)
         ShowErrorMsg(ErrorMsg)
         ShowAppInfo(AppInfo)
     End Sub
 
+    ''' <summary>
+    ''' Zeigt eine Fehlermeldung in der Konsole an.
+    ''' </summary>
+    ''' <param name="ErrorMsg">Die anzuzeigende Fehlermeldung.</param>
     Public Shared Sub ShowErrorMsg(ErrorMsg As String)
         Console.WriteLine($"{ErrorMsg}")
     End Sub
 
+    ''' <summary>
+    ''' Zeigt eine Hilfemeldung in der Konsole an.
+    ''' </summary>
+    ''' <param name="HelpMsg">Die anzuzeigende Hilfemeldung.</param>
     Public Shared Sub ShowHelpMsg(HelpMsg As String)
         Console.WriteLine($"{HelpMsg}")
     End Sub
 
+    ''' <summary>
+    ''' Zeigt den Programmheader mit Name, Version und Copyright-Informationen in der
+    ''' Konsole an.
+    ''' </summary>
+    ''' <param name="AppInfo">Die Assembly-Informationen der Anwendung.</param>
     Public Shared Sub ShowAppInfo(AppInfo As AssemblyInfo)
         Dim name = AppInfo.AssemblyName
         Dim version = AppInfo.Version.ToString
