26885d25e9b6202809405ba86ec96e95ec191cdd
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-03-08 09:58:31 +0100

    ShowErrorMsg-Aufrufe angepasst, For-Schleife gekürzt
    - Die Argumentreihenfolge bei ConsoleHelper.ShowErrorMsg wurde geändert: Fehlermeldung und Hilfetext sind nun die ersten beiden Parameter, My.Application.Info wird als drittes Argument übergeben.
    - Alle betroffenen Fehlermeldungs-Ausgaben entsprechend angepasst (keine Argumente, ungültiger Zahlenwert, fehlerhaftes Argument).
    - Die For-Next-Schleife zur Ausgabe der Töne (Console.Beep) wurde in eine einzeilige Kurzform umgewandelt.

--- Beep/Programm.vb
+++ Beep/Programm.vb
@@ -17,46 +17,30 @@ Module Programm
 
         ' Anzahl der übergebenen Argumente prüfen
         If Args.Length = 0 Then
-
             ' Fehlermeldung anzeigen wenn keine Argumente angegeben sind	und Ende
-            ConsoleHelper.ShowErrorMsg(My.Application.Info, $"{My.Resources.ErrorMsg_01}", $"{My.Resources.HelpString}")
+            ConsoleHelper.ShowErrorMsg($"{My.Resources.ErrorMsg_01}", $"{My.Resources.HelpString}", My.Application.Info)
             Return
-
         End If
-
         ' Wenn Argumente angegeben wurden -> 1. Argument einlesen
         Dim Arg As String = Args(0)
-
         'Fehlertest des eingelesenen Arguments
         If Arg.StartsWith("/n:", StringComparison.OrdinalIgnoreCase) Then
-
             ' Wert des Arguments ermitteln
             Dim ArgWert As String = Arg.Substring(3)
-
             ' Fehlertest des Argumentwertes
             Dim Wert As Integer
             If Integer.TryParse(ArgWert, Wert) Then
-
                 ' Argumentwert ist gültig -> Wert begrenzen
                 Wert = Math.Max(MinTones, Math.Min(MaxTones, Wert))
-
                 ' Ausgabe der Töne
-                For n As Integer = 0 To Wert
-                    Console.Beep()
-                Next
-
+                For n As Integer = 0 To Wert : Console.Beep() : Next
             Else
-
                 ' Ungültiger Zahlenwert -> Fehlermeldung ausgeben
-                ConsoleHelper.ShowErrorMsg(My.Application.Info, $"{My.Resources.ErrorMsg_02}", $"{My.Resources.HelpString}")
-
+                ConsoleHelper.ShowErrorMsg($"{My.Resources.ErrorMsg_02}", $"{My.Resources.HelpString}", My.Application.Info)
             End If
-
         Else
-
             ' Argument ist fehlerhaft -> Fehlermeldung ausgeben
-            ConsoleHelper.ShowErrorMsg(My.Application.Info, $"{My.Resources.ErrorMsg_03}", $"{My.Resources.HelpString}")
-
+            ConsoleHelper.ShowErrorMsg($"{My.Resources.ErrorMsg_03}", $"{My.Resources.HelpString}", My.Application.Info)
         End If
 
     End Sub
