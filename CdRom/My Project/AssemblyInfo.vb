4898c2d69658a0e31d47c50ed9b9e4ae7f77036a
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-03-08 11:09:23 +0100

    CdRom: Neues Konsolenprojekt für CD/DVD-Laufwerke
    - Neues Projekt "CdRom" (VB.NET, .NET 4.0 Client) zur Solution hinzugefügt
    - Implementierung von Befehlen zum Prüfen, Öffnen und Schließen von CD/DVD-Laufwerken per Kommandozeile
    - Nutzung von MCI-Befehlen (winmm.dll) zur Steuerung des Laufwerks
    - Ressourcen für Fehlermeldungen, Hilfetexte und Statusmeldungen (deutsch) angelegt
    - AssemblyInfo, Settings, Application-Konfiguration und app.config hinzugefügt
    - Solution-Datei um Projekt und Build-Konfigurationen erweitert

---
+++ CdRom/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("CdRom")>
+<Assembly: AssemblyDescription("Kommandozeilentool mit Funktionen für CD-Laufwerke.")>
+<Assembly: AssemblyCompany("privat")>
+<Assembly: AssemblyProduct("CdRom")>
+<Assembly: AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
+<Assembly: AssemblyTrademark("SchlumpfSoft")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("8be09111-c53e-43e8-b36f-1d13f5c7aee0")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.2026.0307.0")>
+<Assembly: AssemblyFileVersion("1.2026.0307.0")>
+<Assembly: NeutralResourcesLanguage("de-DE")>
